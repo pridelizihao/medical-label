91c892fb9c0341d7f2998613f2b0959712e6dcee
pridelizihao <pridelzh@foxmail.com>
2024-12-06 15:58:31 +0800

    01

--- mybloger/author/models.py
+++ mybloger/author/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Captcha(models.Model):
-    email = models.EmailField(unique=True)
-    captcha = models.CharField(max_length=4)
-    create_time = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(unique=True,verbose_name="邮箱")
+    captcha = models.CharField(max_length=4,verbose_name="验证码")
+    create_time = models.DateTimeField(auto_now_add=True,verbose_name="创建时间")
