36b735cf2a2d54954d6c2f6705c806df8a982027
pridelizihao <pridelzh@foxmail.com>
2024-12-06 19:35:23 +0800

    12

--- mybloger/app01/models.py
+++ mybloger/app01/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Base64Image(models.Model):
-    title = models.CharField(max_length=100)
-    image_data = models.TextField()  # 用于存储base64编码的图片数据
- 
+    title = models.CharField(max_length=100, verbose_name="标题")
+    image_data = models.TextField(verbose_name="图片数据")  # 用于存储base64编码的图片数据
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     def __str__(self):
         return self.title
