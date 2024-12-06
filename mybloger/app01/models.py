957345c0f04956cb04aca8c538f32cd2c4bf8094
pridelizihao <pridelzh@foxmail.com>
2024-12-06 21:27:35 +0800

    完成图像数据库的建立

--- mybloger/app01/models.py
+++ mybloger/app01/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Base64Image(models.Model):
     title = models.CharField(max_length=100, verbose_name="标题")
+    image = models.ImageField(upload_to='images/%Y/%m/%d', verbose_name="图片", default='default.jpg')
     image_data = models.TextField(verbose_name="图片数据")  # 用于存储base64编码的图片数据
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     def __str__(self):
